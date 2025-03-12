1740dd9124b03a4aecdbd13e7703cd3613b89518
MariaKrasnaya <mariakrasny@yandex.ru>
2025-03-12 12:08:56 +0300

    п

--- java.js
+++ java.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', () => {
       if (event.buttons === 1) { // Проверяем, нажата ли левая кнопка мыши
           const blurSpot = document.createElement('div');
           blurSpot.classList.add('blur-effect');
-          blurSpot.style.left = `${event.clientX}px`;
-          blurSpot.style.top = `${event.clientY}px`;
+          blurSpot.style.left = `${event.pageX}px`;
+          blurSpot.style.top = `${event.pageY}px`;
           document.body.appendChild(blurSpot);
       }
   });
@@ -27,6 +27,8 @@ document.addEventListener('DOMContentLoaded', () => {
             let iterations = 0;
             interval = setInterval(() => {
                 glitchText.innerText = getRandomString(originalText.length);
+                glitchText.style.transition = "opacity 1s ease-in-out";
+                glitchText.style.userSelect = "none"
                 iterations++;
                 if (iterations > 10) {
                     clearInterval(interval);
@@ -37,5 +39,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
         setInterval(startGlitchEffect, 3000);
 // glitch
+
+
+// Показать popup
+document.getElementById('showPopup').addEventListener('click', function() {
+  document.getElementById('popup').style.display = 'flex';
+});
+
+// Закрыть popup
+document.getElementById('closePopup').addEventListener('click', function() {
+  document.getElementById('popup').style.display = 'none';
+});
+
+// Отправить введенный текст
+document.getElementById('submitInput').addEventListener('click', function() {
+  const inputText = document.getElementById('popupInput').value;
+  alert('Вы ввели: ' + inputText);
+  document.getElementById('popup').style.display = 'none'; // Закрыть popup после отправки
+});
 
 });
